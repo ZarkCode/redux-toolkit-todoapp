9c2491383a8a882da9010686e373efebdd217698
ZarkCode <tejaswork.contactme@gmail.com>
2026-03-04 12:14:23 +0530

    added redux and redux toolkit dependency and store, reduces been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import RecordTable from "./components/RecordTable";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <RecordTable />
-    </div>
+    </Provider>
   );
 };
 
